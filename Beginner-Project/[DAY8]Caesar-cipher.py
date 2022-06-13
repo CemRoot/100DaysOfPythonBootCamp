ec35722c531783115b8460c7b0907f9ebdaa73c4
Cem Koyluoglu <Koyluoglu.Cem@lll.kpi.ua>
2022-06-13 18:05:44 +0300

    BootCamp22

--- Beginner-Project/[DAY8]Caesar-cipher.py
+++ Beginner-Project/[DAY8]Caesar-cipher.py
@@ -1,5 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'Grading_Program.md', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'Grading_Program.md',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']  # Alfabeyi ekledik
 
 
---
+++ [DAY-9]/Dictionary_in_list.py
@@ -0,0 +1,29 @@
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+
+# 🚨 Do NOT change the code above
+
+
+# to be added to the travel_log. 👇
+def add_new_country(country, visits, cities):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visits
+    new_country["cities"] = cities
+    travel_log.append(new_country)
+
+
+# 🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
