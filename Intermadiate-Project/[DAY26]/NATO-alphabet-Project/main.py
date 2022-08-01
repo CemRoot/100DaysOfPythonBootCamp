d2d996590a279b75404f4637c8746e1e38a1d588
Cem koyluoglu <61415601+CemRoot@users.noreply.github.com>
2022-08-01 23:38:16 +0300

    BootCamp22

--- Intermadiate-Project/[DAY26]/NATO-alphabet-Project/main.py
+++ Intermadiate-Project/[DAY26]/NATO-alphabet-Project/main.py
@@ -1,23 +1,23 @@
-student_dict = {
-	"student": ["Angela", "James", "Lily"],
-	"score": [56, 76, 98]
-}
-
 # Looping through dictionaries:
-for (key, value) in student_dict.items():
-	# Access key and value
-	pass
+# student_dict = {
+# 	"student": ["Angela", "James", "Lily"],
+# 	"score": [56, 76, 98]
+#
+# }
+# for (key, value) in student_dict.items():
+# Access key and value
+# pass
 
 import pandas
 
-student_data_frame = pandas.DataFrame(student_dict)
+# student_data_frame = pandas.DataFrame(student_dict)
 
 # Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-	# Access index and row
+# for (index, row) in student_data_frame.iterrows():
+# Access index and row
 
-	# Access row.student or row.score
-	pass
+# Access row.student or row.score
+
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -26,8 +26,17 @@ for (index, row) in student_data_frame.iterrows():
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+	word = input("Enter a word: ").upper()
+	try:
+		output_list = [phonetic_dict[letter] for letter in word]
+	except KeyError:
+		print("Sorry, only letters are allowed.")
+		generate_phonetic()
+	else:
+		print(output_list)
+
+
+generate_phonetic()
--- Intermadiate-Project/[DAY29]/password-manager-project/main.py
+++ Intermadiate-Project/[DAY29]/password-manager-project/main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
@@ -6,6 +7,7 @@ import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
+
 def generate_password():
 	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
 	           'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -25,24 +27,61 @@ def generate_password():
 	pyperclip.copy(password)
 
 
+# ---------------------------- LOAD PASSWORD ------------------------------- #
+def check_password():
+	website = entry_website.get()
+	try:
+		with open("data.json", "r") as file:
+			data = json.load(file)
+	except FileNotFoundError:
+		messagebox.showerror("OOPSS!", "No data found.")
+	else:
+		if website in data:
+			email = data[website]["email"]
+			password = data[website]["password"]
+			messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+		else:
+			messagebox.showerror("OOPSS!", "Website does not exist.")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
 	# Get the values from the entry boxes.
 	website = entry_website.get()
 	username = entry_email_uname.get()
 	password = entry_password.get()
+	new_data = {
+		website: {
+			"email": username,
+			"password": password,
+		}
+	}
 	if len(website) == 0 or len(username) == 0 or len(password) == 0:
 		messagebox.showerror("OOPSS!", "Please fill in all fields.")
 	else:
 
 		# Message box to confirm the password.
-		is_ok = messagebox.askokcancel(title="website",
-		                               message=f"These are the details entered: \nEmail: {username} "
-		                                       f"\nPassword: {password} \nIs this correct?")
-		if is_ok:
-			# Add the dictionary to the list.
-			with open("data.txt", "a") as file:
-				file.write(f"{website} | {username} | {password}\n")
+		# is_ok = messagebox.askokcancel(title="website",
+		#                                message=f"These are the details entered: \nEmail: {username} "
+		#                                        f"\nPassword: {password} \nIs this correct?")
+		# if is_ok:
+		# Add the dictionary to the list.
+		try:
+			with open("data.json", "r") as file:
+				# file.write(f"{website} | {username} | {password}\n")
+				# Reading old data
+				data = json.load(file)
+		except FileNotFoundError:
+			with open("data.json", "w") as file:
+				json.dump(data, file, indent=4)
+		else:
+			# Updating old data with new data
+			data.update(new_data)
+
+			with open("data.json", "w") as file:
+				# Saving update data
+				json.dump(data, file, indent=4)
+		finally:
 			# Clear the entry boxes.
 			entry_website.delete(0, END)
 			# entry_email_uname.delete(0, END)
@@ -63,7 +102,7 @@ label_website = Label(text="Website:")
 label_website.grid(column=0, row=1)
 
 entry_website = Entry()
-entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
+entry_website.grid(column=1, row=1, columnspan=1, sticky="EW")
 
 label_email_uname = Label(text="Email/Username:")
 label_email_uname.grid(column=0, row=2)
@@ -76,11 +115,13 @@ label_password.grid(column=0, row=3)
 
 entry_password = Entry()
 entry_password.grid(column=1, row=3, sticky="EW")
-
+# Buttons
 generate_btn = Button(text="Generate Password", command=generate_password)
 generate_btn.grid(column=2, row=3, sticky="EW")
 
 add_btn = Button(text="Add", width=35, command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
 
+search_btn = Button(text="Search", command=check_password)
+search_btn.grid(column=2, row=1, sticky="EW")
 mainloop()
