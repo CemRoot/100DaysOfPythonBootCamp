dc38e50538448ea3d0b7da553d9f84b4318cd626
Cem koyluoglu <61415601+CemRoot@users.noreply.github.com>
2022-08-21 19:13:46 +0300

    BootCamp22

--- Intermadiate_Plus-Project/[DAY39]/notification_manager.py
+++ Intermadiate_Plus-Project/[DAY39]/notification_manager.py
@@ -1,6 +1,7 @@
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
+import smtplib
 
 load_dotenv()
 
@@ -9,11 +10,24 @@ class NotificationManager:
 
     def __init__(self):
         self.client = Client(os.environ['TWILIO_SID'], os.environ['TWILIO_TOKEN'])
-
+#Whatsapp sender msg
     def send_sms(self, message):
         message = self.client.messages.create(
+            from_='whatsapp:+14155238886',
             body=message,
-            from_=os.environ["TWILIO_NUMBER"],
-            to=os.environ["TWILIO_MY_NUMBER"],
+            to="whatsapp:+380661508739",
         )
         print(message.sid)
+
+                )
+# Mail sender
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
